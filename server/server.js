77f727ee3e523f3f30b361cb965f7cd28efd9751
xarsin <jdj.souza@gmail.com>
2020-10-24 14:38:37 -0500

    add comments to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
+
+// require router js
 const taskRouter = require('./routes/tasks.router');
 
 // Configure body-parser
